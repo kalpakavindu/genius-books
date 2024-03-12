3e568fe15c6b070e905cc075bb5cf8767482a143
kalpakavindu <kalpadevonline@gmail.com>
2024-03-12 13:06:38 +0530

    files added

---
+++ README.md
@@ -0,0 +1,15 @@
+# GeniusBooks
+
+Simple windows application software for manage main processes in a bookshop.
+
+### Tech Stack
+
+- .NET Framework
+- SQL Server
+- C++
+
+## Features
+- Adding a new book.
+- Stock Management.
+- Supplier Management.
+- Login System.
---
+++ SQLQuery.sql
@@ -0,0 +1,51 @@
+﻿CREATE TABLE [dbo].[User] (
+  [id] INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+  [first_name] VARCHAR(20) NOT NULL,
+  [last_name] VARCHAR(20) NOT NULL,
+  [email] VARCHAR(70) UNIQUE NOT NULL,
+  [password] VARCHAR(128),
+  [dob] DATE,
+  [gender] VARCHAR(10),
+  [contact] VARCHAR(15),
+  [role] VARCHAR(10) NOT NULL DEFAULT 'USER',
+  [created_at] TIMESTAMP NOT NULL
+)
+GO
+
+CREATE TABLE [dbo].[Supplier] (
+  [id] INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  [name] VARCHAR(70) NOT NULL,
+  [address] VARCHAR(100) NOT NULL,
+  [email] VARCHAR(70) UNIQUE NOT NULL,
+  [contact] VARCHAR(15) NOT NULL,
+  [created_at] TIMESTAMP NOT NULL
+)
+GO
+
+CREATE TABLE [dbo].[Book] (
+  [isbn] INT PRIMARY KEY NOT NULL,
+  [title] VARCHAR(100) NOT NULL,
+  [author] VARCHAR(70) NOT NULL,
+  [genre] VARCHAR(50) NOT NULL,
+  [publisher] VARCHAR(70) NOT NULL,
+  [unit_price] FLOAT NOT NULL,
+  [stock_qty] INT DEFAULT (0),
+  [supplier] INT NOT NULL,
+  [created_at] TIMESTAMP NOT NULL,
+  CONSTRAINT [FK_Book2Supplier] FOREIGN KEY ([supplier]) REFERENCES [dbo].[Supplier] ([id])
+)
+GO
+
+CREATE TABLE [dbo].[Order] (
+  [id] INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  [user] INT NOT NULL,
+  [supplier] INT NOT NULL,
+  [book] INT NOT NULL,
+  [qty] INT DEFAULT 0,
+  [status] INT DEFAULT 0,
+  [created_at] TIMESTAMP NOT NULL,
+  CONSTRAINT [FK_Order2User] FOREIGN KEY ([user]) REFERENCES [dbo].[User] ([id]),
+  CONSTRAINT [FK_Order2Supplier] FOREIGN KEY ([supplier]) REFERENCES [dbo].[Supplier] ([id]),
+  CONSTRAINT [FK_Order2Book] FOREIGN KEY ([book]) REFERENCES [dbo].[Book] ([isbn])
+)
+GO
